821c071146fb47b8abde5f91a9855ce4346df698
simoneclr <simone.clclr@gmail.com>
2016-04-14 15:26:01 +0200

    Fix error in realizability algorithm; add transducerOutputFunction to produce strategy

--- src/main/java/synthesis/TransitionMap.java
+++ src/main/java/synthesis/TransitionMap.java
@@ -3,6 +3,7 @@ package synthesis;
 import rationals.State;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -11,5 +12,5 @@ import java.util.Set;
  * Created by Simone Calciolari on 14/04/16.
  * @author Simone Calciolari.
  */
-public class TransitionMap extends HashMap<PropositionSet, HashMap<PropositionSet, Set<State>>> {
+public class TransitionMap extends HashMap<PropositionSet, HashSet<State>> {
 }

76b39a1946ea8612b18b1d97a39a500eeeb8de31
simoneclr <simone.clclr@gmail.com>
2016-06-17 11:33:38 +0200

    Add license header to files

--- src/main/java/synthesis/SynthesisAutomaton.java
+++ src/main/java/synthesis/SynthesisAutomaton.java
@@ -24,6 +24,23 @@ import static util.AutomatonUtils.*;
  * <br>
  * Created by Simone Calciolari on 01/04/16.
  * @author Simone Calciolari.
+ *
+ * LTL-Synthesis. Perform LTL Synthesis on finite traces. Copyright (C) 2016 Simone Calciolari
+ *
+ * This file is part of LTL-Synthesis.
+ *
+ * LTL-Synthesis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LTL-Synthesis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LTL-Synthesis. If not, see <http://www.gnu.org/licenses/>.
  */
 public class SynthesisAutomaton {
 
